ec87fc8c2f31391adcbf5b0dd52fa32d17635a94
Umar-DevOps-design <umar.sharief14u@gmail.com>
2024-12-30 18:17:28 +0000

    Initial commit of Dockerized React app

---
+++ bash1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git checkout -b dev
+git add .
+git commit -m "Initial commit of Dockerized React app"
+git remote add origin https://github.com/Umar-DevOps-design/dev
+git push -u origin dev
---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Building the Docker image..."
+docker-compose build
+
